31a63d6747523f74cbefe9f6321d04af300f89ac
guliyayl <guliyayl@fit.cvut.cz>
2022-07-21 13:28:37 +0200

    custom Tinder card component

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import {Text, View, StyleSheet, ImageBackground} from 'react-native';
+import {View, StyleSheet, ImageBackground, Text} from 'react-native';
+import Card from './src/components/TinderCard';
+import users from './assets/data/users';
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-        <ImageBackground
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
-          }}
-          style={styles.image}>
-          <View style={styles.cardInner}>
-            <Text style={styles.name}>YUPPY</Text>
-            <Text style={styles.bio}>White and bald</Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <Card user={users[2]} />
     </View>
   );
 };
@@ -25,40 +16,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-  },
-  card: {
-    width: '95%',
-    height: '75%',
-
-    shadowColor: '#000000',
-    shadowOffset: {
-      width: 0,
-      height: 7,
-    },
-    shadowOpacity: 1,
-    shadowRadius: 7.68,
-    elevation: 10,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-
-    justifyContent: 'flex-end',
-  },
-  cardInner: {
-    padding: 10,
-  },
-  name: {
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  bio: {
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,
   },
 });
 
---
+++ src/components/TinderCard/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Text, StyleSheet, View, ImageBackground} from 'react-native';
+
+const Card = props => {
+  const {name, image, bio} = props.user;
+  return (
+    <View style={styles.card}>
+      <ImageBackground
+        source={{
+          uri: image,
+        }}
+        style={styles.image}>
+        <View style={styles.cardInner}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.bio}>{bio}</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: '95%',
+    height: '75%',
+
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 7.68,
+    elevation: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    color: 'green',
+    fontWeight: 'bold',
+  },
+  bio: {
+    fontSize: 18,
+    color: 'green',
+    lineHeight: 25,
+  },
+});
+
+export default Card;
